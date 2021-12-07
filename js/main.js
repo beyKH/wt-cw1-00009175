73ba59f922554dbab662e0adc78f8af72dc5e07a
beyKH <asadbek.mah@gmail.com>
2021-12-07 13:49:03 +0500

    hero, skills, work sections finished

--- js/main.js
+++ js/main.js
@@ -5,3 +5,46 @@ const elSiteHeader = document.querySelector(".site-header");
 elBtnSitenavToggler.addEventListener("click", function () {
   elSiteHeader.classList.toggle("site-header--open");
 });
+
+
+// ADDING SKILLS TEXT
+const elSkillsItem = document.querySelectorAll(".skills__text");
+let counter = 0;
+elSkillsItem.forEach(item =>{
+  item.textContent = skills[counter];
+  counter+=1;
+});
+
+// WORKING SECTOIN CREATING ELEMENT
+const elWorkList = document.querySelector(".work__list");
+
+for( work of works){
+  li = document.createElement("li");
+  a = document.createElement("a");
+  workWrapper = document.createElement("div");
+  p = document.createElement("p");
+  workShower = document.createElement("div");
+  img = document.createElement("img");
+
+  a.href = work.link;
+  a.target = "_blank";
+  img.src = work.img;
+  p.textContent = work.text;
+
+  li.classList.add("work__item");
+  workWrapper.classList.add("work__wrapper");
+  workShower.classList.add("work__shower");
+  img.classList.add("work__img");
+
+  p.classList.add("work__text");
+
+
+  workWrapper.appendChild(img);
+  workShower.appendChild(p);
+  workWrapper.appendChild(workShower);
+  a.appendChild(workWrapper);
+  li.appendChild(a);
+
+  elWorkList.appendChild(li);
+}
+
---
+++ js/works.js
@@ -0,0 +1,30 @@
+works = [{
+  "img": "img/work1.png",
+  "link": "https://myteam-asadbek.netlify.app/",
+  "text": "Myteam"},
+  {
+    "img": "img/work2.png",
+    "link": "https://payapi-website-asadbek.netlify.app/",
+    "text": "PayApi",
+  },
+  {
+    "img": "img/work3.png",
+    "link": "https://sunny-side-asadbek.netlify.app/",
+    "text": "Sunnyside",
+  },
+  {
+  "img": "img/work4.png",
+  "link": "https://coffee-roaster-final-asadbek.netlify.app/",
+  "text": "CoffeRoaster"},
+  {
+    "img": "img/work5.png",
+    "link": "https://easy-bank-asadbek.netlify.app/",
+    "text": "EasyBank",
+  },
+  {
+    "img": "img/work6.png",
+    "link": "https://bookmark-netlify.netlify.app/",
+    "text": "Bookmark",
+  },
+]
+
